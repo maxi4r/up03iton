937c8f2db6e3a4ae0c22cfe305b369c9d69ac32d
Maximilian Riehl <maximilian.riehl@fau.de>
2021-11-09 21:04:50 +0100

    add debugging output

--- up03iton/function.py
+++ up03iton/function.py
@@ -9,9 +9,11 @@ def imshow(X, resize=None):
     We should be able to install this package in Google Colab from your Git repo.
     """
     im = Image.fromarray(X)
+    print("converted image")
     if resize:
         # assuming resize is a tuple spedifing the new size
         im.resize(resize)
+    print("resized image")
     # assuming the function is supposed to also show the image since its name is imshow.
     im.show()
 
