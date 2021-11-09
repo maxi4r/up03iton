a3990fd75bc5ba839fb58027486ad69683a30d3d
Maximilian Riehl <maximilian.riehl@fau.de>
2021-11-09 20:38:28 +0100

    comments: committing my first package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='up03iton',
+        version='0.1'
+        author='DSSS',
+        author_email=''
+        packages=find_packages(),
+        install_requires=['numpy', 'Pillow', 'ipywidgets'])
---
+++ up03iton/function.py
@@ -0,0 +1,17 @@
+import numpy as np
+from ipywidgets import interact, fixed
+from PIL import Image
+
+def imshow(X, resize=None):
+    """
+    You should create a way to resize an image from an array X.
+    The use of widgets is optional but you can take a look to interact.
+    We should be able to install this package in Google Colab from your Git repo.
+    """
+    im = Image.fromarray(X)
+    if resize:
+        # assuming resize is a tuple spedifing the new size
+        im.resize(resize)
+    # assuming the function is supposed to also show the image since its name is imshow.
+    im.show()
+
